4ad0d4faa8db3f7e2d1ab054f3d4c18e8a46e0db
jachinlin <jachin1992@hotmail.com>
2018-05-12 14:54:35 +0800

    toc html template

--- maker.py
+++ maker.py
@@ -17,6 +17,10 @@ def render_toc_ncx(first_level_post_list):
     render_file('toc.xml', {'first_level_post_list': first_level_post_list}, 'toc.ncx', output_dir)
 
 
+def render_toc_html(first_level_post_list):
+    render_file('toc.html', {'first_level_post_list': first_level_post_list}, 'toc.html', output_dir)
+
+
 if __name__ == '__main__':
     first_level_post_list = [
         {
@@ -38,6 +42,11 @@ if __name__ == '__main__':
             ]
         },
     ]
-    render_toc_ncx(first_level_post_list)
 
+    render_toc_ncx(first_level_post_list)
     print('test render_toc_ncx pass')
+
+    render_toc_html(first_level_post_list)
+    print('test render_toc_html pass')
+
+
