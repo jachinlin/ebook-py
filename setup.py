51b05fa972729c87adde105b4fbc0889faa906ba
jachinlin <jachin1992@hotmail.com>
2018-05-18 21:49:09 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from setuptools import setup
+
+setup(
+    name='kindle_maker',
+    version='0.0.1',
+    author='jachinlin',
+    author_email='linjx1000@gmail.com',
+    url='https://github.com/jachinlin/kindle_maker',
+    description='a tool make kindle mobi ebook',
+    license='MIT',
+    keywords='kindle ebook mobi',
+    packages=[],
+    install_requires=[
+        'Jinja2==2.10'
+    ],
+    entry_points={}
+)
