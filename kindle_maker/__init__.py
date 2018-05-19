ec74c49b74cff9e8434aab599de48acf40a7fd1e
jachinlin <jachin1992@hotmail.com>
2018-05-20 00:17:10 +0800

    fix templates path

--- kindle_maker/__init__.py
+++ kindle_maker/__init__.py
@@ -1,5 +1,18 @@
 # coding=utf8
 
+import sys
 from .ebook_maker import make_ebook as make_mobi
 
 
+def make_mobi_command():
+    args = sys.argv[1:]
+    if len(args) < 2:
+        print("""make_mobi usage:
+1. prepare html files and a toc.md file in a source dir, and then call 
+2. make_mobi <source_dir> <output_dir>
+        """)
+        return
+
+    make_mobi(args[0], args[1])
+
+    print("success")
