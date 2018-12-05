5a527a93fd99dd6d085542b63bf6352dd0640f4c
jachinlin <jachin1992@hotmail.com>
2018-12-05 23:54:07 +0800

    update opf.mml

--- examples/run.py
+++ examples/run.py
@@ -1,4 +1,7 @@
+# coding=utf8
+import sys
+sys.path.append('.')
 from kindle_maker.ebook_maker import make_ebook
 
 if __name__ == '__main__':
-    make_ebook('./source', '.')
+    make_ebook('./examples/source', '.')
